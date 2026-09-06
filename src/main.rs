f2f978564ea13f15987d5293d698b99fe543e4ee
headless-axolotl <martin.kostadinov05@gmail.com>
2026-09-06 22:11:55 +0300

    small experiment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("greetings");
+use rocket::{Responder, get, launch, routes};
+
+#[derive(Responder)]
+#[response(status=200, content_type="html")]
+pub struct Html {
+    text: String,
+}
+
+impl From<String> for Html {
+    fn from(value: String) -> Self {
+        Self { text: value }
+    }
+}
+
+#[get("/")]
+fn index() -> Html {
+    String::from("<!DOCTYPE html><html><body><p>Henlo</p></body></html>").into()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
 }
